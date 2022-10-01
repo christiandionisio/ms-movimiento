ee205f32fe900ee64f4d03597fb1639873ce7969
Christian Dionisio <christiandionisio9432@gmail.com>
2022-09-30 22:33:15 -0500

    First commit

---
+++ src/test/java/com/example/msmovimiento/MsMovimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msmovimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMovimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
